b637ffe6b50d0436b53c52f2f95fdd7b103aac5d
Gabi Oliveira <gabriela.gomestra@gmail.com>
2025-07-19 19:15:22 -0300

    findById implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,5 +1,6 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
@@ -7,6 +8,6 @@ public class DaoFactory {
 //     porém internamente ela vai instanciar uma implemetnação
 //    macete para não expor a impementação.
     public static SellerDao createSellerDao() {
-        return new SellerDaoJDBC();
+        return new SellerDaoJDBC(DB.getConnection());
     }
 }
